db1838b3fc5e472e720d89b9d9a45e486cb03157
Jamund Ferguson <jamund@gmail.com>
2012-06-05 17:14:30 -0600

    Added a simple example and documentation about how to use it

--- grunt.js
+++ grunt.js
@@ -2,13 +2,18 @@ module.exports = function (grunt) {
 	// Project configuration.
 	grunt.initConfig({
 		lint: {
-			all: [ "src/**/*.js" ]
+			all: [ "src/**/*.js" ],
+			examples: [ "example/**/*.js" ]
 		},
 
 		test: {
 			all: [ "test/unit/**/*.js" ]
 		},
-
+		
+		examples: {
+			jquery: "jquery.js"
+		},
+		
 		jshint: {
 			options: {
 				"es5": true,
@@ -22,4 +27,8 @@ module.exports = function (grunt) {
 	});
 
 	grunt.registerTask("default", "lint test");
+	grunt.registerMultiTask("examples", "Run examples", function () {
+		var done = this.async();
+		require("./example/" + this.data).run(done);
+	});
 };
